dc557c35af6a037becd86423ae218c01f912633f
hong kim <hongkyukim@yahoo.com>
2012-11-24 21:25:39 -0800

    asset compile true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ LoginApp2::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)   default false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed   default false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
